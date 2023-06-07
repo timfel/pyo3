ca31cc7e18418827598beda82a49f4f9cc6d994d
Tim Felgentreff <tim.felgentreff@oracle.com>
2023-06-07 16:03:20 +0200

    graalpy: add implementations for list macro functions

--- src/ffi/cpython/listobject.rs
+++ src/ffi/cpython/listobject.rs
@@ -1,6 +1,7 @@
 use crate::ffi::object::*;
 use crate::ffi::pyport::Py_ssize_t;
 
+#[cfg(not(any(PyPy, GraalPy)))]
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct PyListObject {
@@ -15,14 +16,14 @@ pub struct PyListObject {
 
 /// Macro, trading safety for speed
 #[inline]
-#[cfg(not(PyPy))]
+#[cfg(not(any(PyPy, GraalPy)))]
 pub unsafe fn PyList_GET_ITEM(op: *mut PyObject, i: Py_ssize_t) -> *mut PyObject {
     *(*(op as *mut PyListObject)).ob_item.offset(i as isize)
 }
 
 /// Macro, *only* to be used to fill in brand new lists
 #[inline]
-#[cfg(not(PyPy))]
+#[cfg(not(any(PyPy, GraalPy)))]
 pub unsafe fn PyList_SET_ITEM(op: *mut PyObject, i: Py_ssize_t, v: *mut PyObject) {
     *(*(op as *mut PyListObject)).ob_item.offset(i as isize) = v;
 }
@@ -30,5 +31,8 @@ pub unsafe fn PyList_SET_ITEM(op: *mut PyObject, i: Py_ssize_t, v: *mut PyObject
 #[inline]
 #[cfg(not(PyPy))]
 pub unsafe fn PyList_GET_SIZE(op: *mut PyObject) -> Py_ssize_t {
-    Py_SIZE(op)
+    #[cfg(not(GraalPy))]
+    return Py_SIZE(op);
+    #[cfg(GraalPy)]
+    return _Py_SIZE(op as *mut PyVarObject);
 }
